ea2c47e2a8e01665efa212d23a41b1d49d46251a
Eduardo Barbet <eduujr2607@gmail.com>
2025-05-29 15:15:45 -0500

    Added example Firebase config

---
+++ src/utils/firebase-config.example.js
@@ -0,0 +1,9 @@
+// src/utils/firebase-config.example.js
+export const firebaseConfig = {
+  apiKey: "YOUR_API_KEY_HERE",
+  authDomain: "up-to-do.firebaseapp.com",
+  projectId: "up-to-do",
+  storageBucket: "up-to-do.firebasestorage.app",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
