a9b8b2587e6a1238242a0b35e834ffd76e677e28
Eduardo Barbet <eduujr2607@gmail.com>
2025-06-03 22:28:44 -0500

    Firebase config

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
 // src/index.js
-//This file will call all the functions inside other files!
 import { showLoginPage } from "./pages/loginPage.js";
+import { showDashboard } from "./pages/dashboardPage.js";
+import { onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+import { auth } from "./services/firebase.js";
 
 const root = document.getElementById("app");
 
-showLoginPage(root, (user) => {
-  root.innerHTML = `<h2>Bienvenido, ${user.email}!</h2>`;
+function renderApp(user) {
+  if (user) {
+    showDashboard(root, user, async () => {
+      await signOut(auth);
+      renderApp(null); // Go back to login on logout
+    });
+  } else {
+    showLoginPage(root, (user) => {
+      renderApp(user); // Go to dashboard on login
+    });
+  }
+}
+
+// Check if user is already logged in
+onAuthStateChanged(auth, (user) => {
+  renderApp(user);
 });
