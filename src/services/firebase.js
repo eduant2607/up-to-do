f4074e26dc6bdf99043c833487f4f18ee89d0ed9
Eduardo Barbet <eduujr2607@gmail.com>
2025-06-03 22:44:51 -0500

    a

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -8,7 +8,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 import { firebaseConfig } from "../utils/firebase-config.js";
-
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
